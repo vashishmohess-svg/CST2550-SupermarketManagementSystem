e995a018bdf873d5dc785c3cf8a779d645a9c6bd
vashishmohess-svg <vashishmohess@gmail.com>
2026-07-01 16:50:47 +0400

    Added Suppliers table

--- Database/CreateDatabase.sql
+++ Database/CreateDatabase.sql
@@ -14,3 +14,17 @@ CREATE TABLE Categories
     CategoryName VARCHAR(100) NOT NULL
 );
 GO
+
+CREATE TABLE Suppliers
+(
+    SupplierID INT IDENTITY(1,1) PRIMARY KEY,
+
+    SupplierName VARCHAR(150) NOT NULL,
+
+    Phone VARCHAR(20),
+
+    Email VARCHAR(100),
+
+    Address VARCHAR(255)
+);
+GO
