09cf87220499188cde303ce345c9b9014de5ad46
vashishmohess-svg <vashishmohess@gmail.com>
2026-07-01 19:54:25 +0400

    Created SeedData.sql and added categories and suppliers

---
+++ Database/SeedData.sql
@@ -0,0 +1,23 @@
+-- Categories
+INSERT INTO Categories (CategoryName)
+VALUES
+('Beverages'),
+('Dairy'),
+('Bakery'),
+('Household'),
+('Personal Care');
+
+-- Suppliers
+INSERT INTO Suppliers
+(
+SupplierName,
+Phone,
+Email,
+Address
+)
+VALUES
+('ABC Distributors','52541234','abc@suppliers.mu','Port Louis'),
+('Island Foods Ltd','57445566','islandfoods@suppliers.mu','Curepipe'),
+('Fresh Mart Supply','59887766','freshmart@suppliers.mu','Vacoas'),
+('Quality Traders','57771122','quality@suppliers.mu','Rose Hill'),
+('Sunrise Wholesale','59881100','sunrise@suppliers.mu','Quatre Bornes');
