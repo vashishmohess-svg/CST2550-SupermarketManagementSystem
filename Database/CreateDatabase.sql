e6ec3d84538b07420da4a35488ec6209facf3656
vashishmohess-svg <vashishmohess@gmail.com>
2026-07-01 16:29:30 +0400

    Added Categories table

--- Database/CreateDatabase.sql
+++ Database/CreateDatabase.sql
@@ -3,3 +3,14 @@ BEGIN
     CREATE DATABASE SupermarketDB;
 END
 GO
+
+USE SupermarketDB;
+GO
+
+CREATE TABLE Categories
+(
+    CategoryID INT IDENTITY(1,1) PRIMARY KEY,
+
+    CategoryName VARCHAR(100) NOT NULL
+);
+GO
